22cb0f3e764949e8804823f40339deff550b8206
Иван Якимов <sasha2020419@gmail.com>
2023-10-27 22:15:19 +0300

    Сonfigure main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,22 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+import store from "./assets/store";
+import VueMeta from "vue-meta";
 
 Vue.config.productionTip = false
 
+Vue.use(VueMeta, {
+  keyName: "metaInfo",
+  attribute: "data-vue-meta",
+  ssrAttribute: "data-vue-meta-server-rendered",
+  tagIDKeyName: "vmid",
+  refreshOnceOnNavigation: true
+});
+Vue.config.productionTip = false;
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
